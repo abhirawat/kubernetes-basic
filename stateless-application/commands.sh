1e9de1c308f53751a4d622fd2c92cb65f282a1e2
Abhishek Rawat <abhi.rot@gmail.com>
2018-08-11 17:16:59 +0530

    Refined image creating commands a bit

--- stateless-application/commands.sh
+++ stateless-application/commands.sh
@@ -1,4 +1,4 @@
-docker build -t stateless-application .
+docker build -t rawat/stateless-application .
 # Test using docker run -it --rm --name stateless-application stateless-application
-sudo docker tag stateless-application:latest stateless-application:1.0
-docker push rawat/stateless-application
+sudo docker tag rawat/stateless-application:latest rawat/stateless-application:1.0
+docker push rawat/stateless-application:1.0
