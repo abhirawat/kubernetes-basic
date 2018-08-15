9acb219cd8ebd4425dd42915736cb15f1dfe941f
Abhishek Rawat <abhi.rot@gmail.com>
2018-08-15 14:26:13 +0530

    Added redis service config instead of command

--- stateless-application/commands.sh
+++ stateless-application/commands.sh
@@ -1,4 +1,10 @@
-docker build -t rawat/stateless-application .
-# Test using docker run -it --rm --name stateless-application stateless-application
-sudo docker tag rawat/stateless-application:latest rawat/stateless-application:1.0
-docker push rawat/stateless-application:1.0
+#!/bin/bash
+
+## Application built using following commands
+#docker build -t rawat/stateless-application .
+#sudo docker tag rawat/stateless-application:latest rawat/stateless-application:1.0
+#docker push rawat/stateless-application:1.0
+
+kubectl create -f 01-app
+kubectl create -f 02-app-ing
+kubectl create -f 03-autoscale
